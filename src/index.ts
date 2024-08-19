dfd9551ee6cdb5519af047ac41da670881017034
Kotaro <swdevstudio@gmail.com>
2024-08-19 13:24:17 +0500

    Add defineError in index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import SowForm from "@/components/SowForm.vue";
 import useField from "@/compose/useField";
 import defineRule from "@/utils/defineRule";
+import defineError from "@/utils/defineError";
 
-export { SowForm, useField, defineRule };
+export { SowForm, useField, defineRule, defineError };
